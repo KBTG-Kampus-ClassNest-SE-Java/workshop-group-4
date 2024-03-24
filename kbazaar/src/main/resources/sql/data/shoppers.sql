8955c1d2909b3753975896c54506b23f218282be
Bank <Bank>
2024-03-24 13:37:57 +0700

    add sql init

--- kbazaar/src/main/resources/sql/data/shoppers.sql
+++ kbazaar/src/main/resources/sql/data/shoppers.sql
@@ -3,3 +3,9 @@ INSERT INTO shopper (username, email) VALUES ('CodeMaster', 'codemaster@example.
 INSERT INTO shopper (username, email) VALUES ('DataGuru', 'dataguru@example.com') ON CONFLICT DO NOTHING;
 INSERT INTO shopper (username, email) VALUES ('CyberSavvy', 'cybersavvy@example.com') ON CONFLICT DO NOTHING;
 INSERT INTO shopper (username, email) VALUES ('GeekChic', 'geekchic@example.com') ON CONFLICT DO NOTHING;
+
+INSERT INTO cart (shopper_id) VALUES (1) ON CONFLICT DO NOTHING;
+INSERT INTO cart (shopper_id) VALUES (2) ON CONFLICT DO NOTHING;
+INSERT INTO cart (shopper_id) VALUES (3) ON CONFLICT DO NOTHING;
+INSERT INTO cart (shopper_id) VALUES (4) ON CONFLICT DO NOTHING;
+INSERT INTO cart (shopper_id) VALUES (5) ON CONFLICT DO NOTHING;
---
+++ kbazaar/src/main/resources/sql/schema/cart.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS cart (
+    id SERIAL PRIMARY KEY,
+    shopper_id INT8 NOT NULL
+);
